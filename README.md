2ffcca24ee8caed26d8062cfde1f089cc244af18
Matheus_k <matheusklauck8@gmail.com>
2022-02-23 09:51:29 -0300

    just saving the code

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # dtmoney
 Front-end desenvolvido para fins de estudo da tecnologia react.js
+
+--
+
+Exemplos de:
+    -   Uso da context api
+    -   Teste da package Mirage.js
+    -   React hooks
+    -   Programação funcional
+
+[en-US]
+
+Developed front-end code for studying react.js
+
+--
+
+Included examples:
+    -   Context api use case
+    -   Mirage.js package testing
+    -   React hooks
+    -   Functional programming
---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import { Dashboard } from "./components/Dashboard";
+import { Header } from "./components/Header";
+import { NewTransactionModal } from "./components/NewTransactionModal";
+import { GlobalStyle } from "./styles/global";
+import { TransactionsProvider } from "./hooks/useTransactions";
+
+export function App() {
+  const [isNewTransactionModalOpen, setIsTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsTransactionModalOpen(false);
+  }
+
+  return (
+    <TransactionsProvider>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+      <Dashboard />
+      <NewTransactionModal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionModal} />
+      <GlobalStyle />
+    </TransactionsProvider>
+
+
+  );
+}
+
